379f3e8778f03e07d6e4fa1683ce13d027bf9693
Max Chang <maxwellchang@ucsb.edu>
2025-10-24 13:33:05 -0700

    deleted comment

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -4,7 +4,6 @@ import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/** The UCSBDateRepository is a repository for UCSBDate entities. */
 @Repository
 public interface RecommendationRequestRepository
     extends CrudRepository<RecommendationRequest, Long> {}
